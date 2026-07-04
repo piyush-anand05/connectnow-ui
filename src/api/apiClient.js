a619e6e5e03d5ab0b666b130017d31b588400144
piyush-anand05 <124147030+piyush-anand05@users.noreply.github.com>
2026-07-05 03:24:52 +0530

    Update apiClient.js

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://tom-todd-shortcuts-berlin.trycloudflare.com/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export function getToken() {
   return localStorage.getItem("token");
